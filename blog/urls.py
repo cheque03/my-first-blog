134a9e8b5c1cf57894c892bfbf5feac12eb126b5
Ezequiel <cheeke300@gmail.com>
2022-10-05 14:48:33 -0500

    cambios

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url('', views.post_list)
+    url('', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    
 
 ]
+
